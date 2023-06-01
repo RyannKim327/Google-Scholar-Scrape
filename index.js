297d8d0d67974af2cafb677ddae6505536e3ba00
Mr.RySes <mpoprev2@email.com>
2023-06-02 03:33:42 +0800

    06-02-23 03:33 [Fixing Minor Issues]

--- index.js
+++ index.js
@@ -4,22 +4,24 @@ const cheerio = require("cheerio")
 module.exports =  async (search) => {
 	let { data } = await axios.get(`https://scholar.google.com/scholar?q=${search.replace(/\s/gi, "+")}`)
 	let $ = cheerio.load(data)
-	let html = $("#gs_res_ccl_mid")
+	let html = $("div[class='gs_r gs_or gs_scl']")
 	let result = []
 	html.each((i, e) => {
 		let elem = $(e)
-		let title = elem.find(".gs_ggsd").find("a")
-		let text_title = title.text()
-		let link_title = title.attr()['href']
-		let citations = elem.find("a[class='gs_or_cit gs_or_btn gs_nph']").next("a")
-		result.push({
-			title: `${i + 1}. ${text_title}`,
-			link: link_title,
-			citation: {
-				cite: citations.text(),
-				link: `https://scholar.google.com${citations.attr()['href']}`
-			}
-		})
+		let title = elem.find("div[class='gs_or_ggsm']").find("a")
+		try{
+			let text_title = title.text()
+			let link_title = title.attr()['href']
+			let citations = elem.find("a[class='gs_or_cit gs_or_btn gs_nph']").next("a")	
+			result.push({
+				title: `${i + 1}. ${text_title}`,
+				link: link_title,
+				citation: {
+					cite: citations.html(),
+					link: `https://scholar.google.com${citations.attr()['href']}`
+				}
+			})
+		}catch(e){}
 	})
 	console.log(result)
 }
