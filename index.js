48ed3c553b4fc081e35049f8a5fb70d091467fde
Mr.RySes <mpoprev2@email.com>
2023-06-02 16:10:08 +0800

    06-02-23 16:10

--- index.js
+++ index.js
@@ -1,35 +1,49 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
 
+let getCitation = async (id) => {
+	let { data } = await axios.get(`https://scholar.google.com/scholar?q=info:${id}:scholar.google.com/&output=cite`)
+	let $ = cheerio.load(data)
+	let html = $("#gs_citt")
+	console.log(html.html())
+}
+
 let url =  async (url) => {
 	let { data } = await axios.get(url)
 	let $ = cheerio.load(data)
+	let capcha = $("#gs_captcha_ccl")
 	let html = $("div[class='gs_r gs_or gs_scl']")
 	let result = []
-	html.each((i, e) => {
+	console.log(capcha.html())
+	await html.each(async (i, e) => {
 		let elem = $(e)
 		let title = elem.find("div[class='gs_or_ggsm']").find("a")
+		let citations = await getCitation(elem.attr()['data-cid'])
+		result.push(elem.attr()['data-cid'])
 		try{
 			let text_title = elem.find('.gs_rt').text()
 			let link_title = title.attr()['href']
 			let source = elem.find(".gs_a").text()
 			let shortDescription = elem.find(".gs_rs").text()
-			let citations = elem.find("a[class='gs_or_cit gs_or_btn gs_nph']").next("a")
-			let related_articles = citations.next("a")
+			let cites = elem.find("a[class='gs_or_cit gs_or_btn gs_nph']").next("a")
+			let related_articles = cites.next("a")
 			result.push({
 				title: `${text_title}`,
 				shortDescription: shortDescription,
 				source: source,
 				url: link_title,
-				citation: {
-					cite: citations.text(),
-					url: `https://scholar.google.com${citations.attr()['href']}`
+				citation: citations,
+				cites: {
+					cite: cites.text(),
+					url: `https://scholar.google.com${cites.attr()['href']}`
 				},
 				ralatedArticles: {
-					url:`https://scholar.google.com${ related_articles.attr()['href']}`
+					url:`https://scholar.google.com${related_articles.attr()['href']}`
 				}
 			})
-		}catch(e){}
+		}catch(e){
+			console.log(e)
+		}
 	})
 	return result
 }
