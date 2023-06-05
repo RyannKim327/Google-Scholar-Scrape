efcd7b8eafad23a0e406869160b8b47b10307a09
Mr.RySes <mpoprev2@email.com>
2023-06-05 16:07:08 +0800

    06-05-23 16:07

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 const axios = require("axios")
 const cheerio = require("cheerio")
 
-let getCitation = async (id) => {
-	let { data } = await axios.get(`https://scholar.google.com/scholar?q=info:${id}:scholar.google.com/&output=cite`)
+const ua = async () => {
+	let { data } = await axios.get("https://gist.githubusercontent.com/pzb/b4b6f57144aea7827ae4/raw/cf847b76a142955b1410c8bcef3aabe221a63db1/user-agents.txt")
+	let res = data.split(/\n/gi)
+	return res[Math.floor(Math.random() * res.length)]
+}
+
+let getCitation = async (agent, id) => {
+	let { data } = await axios.get(`https://scholar.google.com/scholar?q=info:${id}:scholar.google.com/&output=cite`, {
+		Headers: {
+			'User-Agent': agent
+		}
+	})
 	let $ = cheerio.load(data)
 	let html = $("#gs_citt")
 	let result = {}
 	html.each((i, e) => {
 		let key = $(e).find(".gs_cith")
 		let val = $(e).find(".gs_citr")
-		console.log(key)
-		console.log(val)
+		result[key] = val
 	})
-	console.log(result)
+	return result
 }
 
-let url =  async (url) => {
-	let { data } = await axios.get(url)
+let url =  async (agent, url) => {
+	if(url == undefined){
+		url = agent
+		agent = await ua()
+	}
+	let { data } = await axios.get(url, {
+		Headers: {
+			'User-Agent': agent
+		}
+	})
 	let $ = cheerio.load(data)
 	let html = $("div[class='gs_r gs_or gs_scl']")
 	let result = []
+	if(html.html() == null){
+		return {
+			"status": 404,
+			"message": "There's a capcha for now, please try again later"
+		}
+	}
 	await html.each(async (i, e) => {
 		let elem = $(e)
 		let title = elem.find("div[class='gs_or_ggsm']").find("a")
-		let citations = await getCitation(elem.attr()['data-cid'])
+		let citations = await getCitation(agent, elem.attr()['data-cid'])
 		result.push(elem.attr()['data-cid'])
 		try{
 			let text_title = elem.find('.gs_rt').text()
@@ -48,11 +71,15 @@ let url =  async (url) => {
 			})
 		}catch(e){}
 	})
-	return result
+	return {
+		"status": 200,
+		"data": result
+	}
 }
 
 let search = async (search) => {
-	let data = await url(`https://scholar.google.com/scholar?q=${search.replace(/\s/gi, "+")}`)
+	let agent = await ua()
+	let data = await url(agent, `https://scholar.google.com/scholar?q=${search.replace(/\s/gi, "+")}`)
 	return data
 }
 
