4208afb847b971409f42fae6a96e8877111d9ce2
RyannKim327 <mpoprev2@email.com>
2023-09-23 13:53:45 +0800

    9/23/2023 1:53:45 PM [Auto Git]

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const ua = async () => {
 	return res[Math.floor(Math.random() * res.length)]
 }
 
-let getCitation = async (agent, id) => {
+let getCitation = async (id, agent) => {
 	if(agent == undefined){
 		agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
 	}
--- test.js
+++ test.js
@@ -14,7 +14,7 @@ let b = async () => {
 	// let d = await a.url(c[0].query) // 'https://scholar.google.com/scholar?q=related:0YuT_-rgrZEJ:scholar.google.com/&scioq=stress+management&hl=en&oe=ASCII&as_sdt=0,5')
 	// console.log(d)
 	console.log(c.data[0].citeID)
-	let e = await a.getCitation("", c.data[0].citeID)
+	let e = await a.getCitation(c.data[0].citeID)
 	console.log(e)
 }
 
