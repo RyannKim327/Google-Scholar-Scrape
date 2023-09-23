31b412963981d1db44aa86d21eebc61b881254c0
RyannKim327 <mpoprev2@email.com>
2023-09-23 13:53:25 +0800

    9/23/2023 1:53:25 PM [Auto Git]

--- index.js
+++ index.js
@@ -8,9 +8,12 @@ const ua = async () => {
 }
 
 let getCitation = async (agent, id) => {
+	if(agent == undefined){
+		agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+	}
 	let { data } = await axios.get(`https://scholar.google.com/scholar?q=info:${id}:scholar.google.com/&output=cite&scirp=${id}&hl=en`,{
 		Headers: {
-			"User-Agents": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
+			"User-Agents": agent
 		}
 	})
 	let $ = cheerio.load(data)
